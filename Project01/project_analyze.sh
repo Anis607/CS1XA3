893a71b97a57380ece663eb7c21b0c973bf6114a
tariqm16 <tairqm16@mcmaster.ca>
2019-02-19 02:30:22 +0000

    Update Project 1

--- Project01/project_analyze.sh
+++ Project01/project_analyze.sh
@@ -2,7 +2,7 @@ count1=$(ls -l *.html | wc -l)
 count2=$(ls -l *.js | wc -l)
 count3=$(ls -l *.css | wc -l)
 count4=$(ls -l *.py | wc -l)
-count5=$(ls -l *.hi | wc -l)
+count5=$(ls -l *.hs | wc -l)
 count6=$(ls -l *.sh | wc -l)
 
 echo -e "You have $count1 HTML files"

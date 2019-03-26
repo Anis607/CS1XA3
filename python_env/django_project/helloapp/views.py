fc137b33136175e41120e1ff2b7262cadb12d406
tariqm16 <tairqm16@mcmaster.ca>
2019-03-26 03:48:31 +0000

    Lab07 Finished

---
+++ python_env/django_project/helloapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.urls import path
+from django.contrib import admin
+
+def postRequest(request):
+	user = request.POST.get("name",")
+	password = request.POST.get("password","")
+
+	if user == "Jimmy" and password == "Hendrix":
+		return HttpResponse("Cool")
+	else:
+		return HttpResponse("Bad User Name")
+
